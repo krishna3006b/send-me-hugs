36979a1ffa3d06216f508c668569049b19321417
Krishna Varshney <krishna.varshney@salescode.ai>
2025-04-14 08:49:46 +0530

    Add asset exports and component index files

---
+++ client/components/index.js
@@ -0,0 +1,45 @@
+import Navbar from "./navbar/Navbar";
+import Hero from "./hero/Hero";
+import Footer from "./footer/Footer";
+import FooterBottom from "./footer/FooterBottom";
+import DonorCard from "./cards/DonorCard";
+import Alert from "./alert/Alert";
+import Input from "./input/Input";
+import Form1 from "./fundraising/Form1";
+import Form2 from "./fundraising/Form2";
+import Form3 from "./fundraising/Form3";
+import Form4 from "./fundraising/Form4";
+import Form5 from "./fundraising/Form5";
+import Form6 from "./fundraising/Form6";
+import Form7 from "./fundraising/Form7";
+import Form8 from "./fundraising/Form8";
+import BackButton from "./fundraising/BackButton";
+import ContinueButton from "./fundraising/ContinueButton";
+import DeleteFundraiserPopup from './popups/DeleteFundraiserPopup'
+import SideNav from "./dashboard/SideNav";
+import FundraiserItem from "./dashboard/FundraiserItem";
+import TransactionsTable from "./dashboard/TransactionTable";
+
+export {
+    TransactionsTable,
+    FundraiserItem,
+    SideNav,
+    DeleteFundraiserPopup,
+    Navbar,
+    Hero,
+    Footer,
+    FooterBottom,
+    DonorCard,
+    Alert,
+    Input,
+    Form1,
+    Form2,
+    Form3,
+    Form4,
+    Form5,
+    Form6,
+    Form7,
+    Form8,
+    BackButton,
+    ContinueButton,
+}
